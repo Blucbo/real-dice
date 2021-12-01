6d7a71f4b66c48c9620707f28a485fddd99cc385
Dmytro Baida <dmytrobaida@Dmytros-MacBook-Air.local>
2021-12-01 13:35:13 +0200

    adds secretjs lib

--- src/app/core/services/identity.service.ts
+++ src/app/core/services/identity.service.ts
@@ -1,20 +1,92 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
+import { SigningCosmWasmClient } from "secretjs";
+import { OfflineSigner } from 'secretjs/types/wallet';
+
+// TODO: move to another folder with app configuration
+const SecretNetworkConfig = {
+  chainId: 'holodeck-2',
+  chainName: 'Secret Testnet',
+  rpc: 'http://chainofsecrets.secrettestnet.io:26657',
+  rest: 'https://chainofsecrets.secrettestnet.io',
+  bip44: {
+    coinType: 529,
+  },
+  coinType: 529,
+  stakeCurrency: {
+    coinDenom: 'SCRT',
+    coinMinimalDenom: 'uscrt',
+    coinDecimals: 6,
+  },
+  bech32Config: {
+    bech32PrefixAccAddr: 'secret',
+    bech32PrefixAccPub: 'secretpub',
+    bech32PrefixValAddr: 'secretvaloper',
+    bech32PrefixValPub: 'secretvaloperpub',
+    bech32PrefixConsAddr: 'secretvalcons',
+    bech32PrefixConsPub: 'secretvalconspub',
+  },
+  currencies: [
+    {
+      coinDenom: 'SCRT',
+      coinMinimalDenom: 'uscrt',
+      coinDecimals: 6,
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: 'SCRT',
+      coinMinimalDenom: 'uscrt',
+      coinDecimals: 6,
+    },
+  ],
+  gasPriceStep: {
+    low: 0.1,
+    average: 0.25,
+    high: 0.4,
+  },
+  features: ['secretwasm'],
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class IdentityService {
-  private _value = new BehaviorSubject('test');
-  public readonly value$: Observable<string> = this._value.asObservable();
+  private consmJsClient!: SigningCosmWasmClient;
+  private _address = new BehaviorSubject<string | null>(null);
 
-  constructor() { }
+  public readonly address$: Observable<string | null> = this._address.asObservable();
 
-  get value(): string {
-    return this._value.getValue();
-  }
+  async connectToWallet() {
+    const currentWindow = window as any;
+    if (!currentWindow.getOfflineSigner || !currentWindow.keplr) {
+      alert("Please install keplr extension");
+      return;
+    }
 
-  setValue(v: string) {
-    this._value.next(v);
+    await currentWindow.keplr.experimentalSuggestChain(SecretNetworkConfig);
+    await currentWindow.keplr.enable(SecretNetworkConfig.chainId);
+
+    const keplrOfflineSigner = currentWindow.getOfflineSigner(SecretNetworkConfig.chainId) as OfflineSigner;
+    const accounts = await keplrOfflineSigner.getAccounts();
+    const address = accounts[0].address;
+
+    this.consmJsClient = new SigningCosmWasmClient(
+      SecretNetworkConfig.rest,
+      address,
+      keplrOfflineSigner,
+      currentWindow.getEnigmaUtils(SecretNetworkConfig.chainId),
+      {
+        init: {
+          amount: [{ amount: '300000', denom: 'uscrt' }],
+          gas: '300000',
+        },
+        exec: {
+          amount: [{ amount: '300000', denom: 'uscrt' }],
+          gas: '300000',
+        },
+      },
+    );
+    this._address.next(address);
   }
 }
