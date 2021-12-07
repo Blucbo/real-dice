f4aa5a5f9660fd490133703bcb635106670bb699
Dmytro Baida <dmytrobaida@outlook.com>
2021-12-07 20:49:15 +0200

    design fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   chainId: 'secretdev-1',
-  rpc: 'https://good-firefox-53.loca.lt',
-  rest: 'https://good-firefox-53.loca.lt',
+  rpc: 'https://hungry-fox-24.loca.lt',
+  rest: 'https://hungry-fox-24.loca.lt',
   daoContractAddress: 'secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg',
   nftContractAddress: 'secret10pyejy66429refv3g35g2t7am0was7ya6hvrzf',
 };
